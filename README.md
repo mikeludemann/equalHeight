b4c6f9ae8776805ef76b7b6689f9c15fbbd20817
Mike Ludemann <mike-ludemann@hotmail.de>
2017-11-09 17:20:52 +0100

    First Commit with all data informations

---
+++ README.md
@@ -0,0 +1,9 @@
+# equalHeight
+
+Set the equal height in all elements in a main / parent element
+
+## Example
+
+```code
+equalHeight(".main",".sub");
+```
---
+++ equalHeight.js
@@ -0,0 +1,14 @@
+function equalHeight(main, submain) {
+    'use strict';
+
+    var elements  = main.find(submain),
+        heights   = [],
+        maximumHeight = null;
+
+    elements.each(function () {
+        $(this).removeAttr('style');
+        heights.push($(this).height());
+    });
+    maximumHeight = Math.max.apply(null, heights);
+    elements.height(maximumHeight);
+}
