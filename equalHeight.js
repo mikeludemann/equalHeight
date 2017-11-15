4b95719a739acf884f518e5d0456da26f47b00d6
Mike Ludemann <mike-ludemann@hotmail.de>
2017-11-16 00:07:00 +0100

    Modify equalHeight.js

--- equalHeight.js
+++ equalHeight.js
@@ -1,14 +1,23 @@
 function equalHeight(main, submain) {
+
     'use strict';
 
-    var elements  = main.find(submain),
+    var elements  = $(main).find(submain),
         heights   = [],
         maximumHeight = null;
 
     elements.each(function () {
+        
         $(this).removeAttr('style');
+
         heights.push($(this).height());
+
     });
+
     maximumHeight = Math.max.apply(null, heights);
+
     elements.height(maximumHeight);
+
+    return maximumHeight;
+
 }
